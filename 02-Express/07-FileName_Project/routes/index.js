b48aac44e2cef5660ff6483cd675f5b6022792d5
krayush1109 <hellotoayush@gmail.com>
2024-05-13 16:48:03 +0530

    read file content, display dir files

--- 02-Express/07-FileName_Project/routes/index.js
+++ 02-Express/07-FileName_Project/routes/index.js
@@ -2,21 +2,26 @@ let path = require('path')
 let fs = require('fs');
 
 var express = require('express');
-const { fstat } = require('fs');
+const { title } = require('process');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   console.log(path.join(__dirname, ".."))
-  const filePath = path.join(__dirname, "..", "public", "uploads")
-  const files = fs.readdirSync(filePath);
+  const folderPath = path.join(__dirname, "..", "public", "uploads")
+  const files = fs.readdirSync(folderPath);
   console.log(files)
-
-  files.forEach((elem) => {
-    console.log(elem)
+  
+  let fileContents = [];
+  
+  files.forEach((fileName, i) => {
+    console.log(fileName, i)
+    
+    fileContents[i] = fs.readFileSync(path.join(folderPath, fileName), "utf-8");
+    console.log("FILE CONTENT", fileContents)
   })
 
-  res.render('index', { title: 'Express', files });
+  res.render('index', { title: 'Express', files, fileContents });
 });
 
 router.post('/create', function (req, res, next) {
@@ -27,14 +32,37 @@ router.post('/create', function (req, res, next) {
   let newFileName = req.body.fileName + req.body.selectExtension;
   let finalPath = path.join(currentWorkingFolder, 'public', 'uploads', newFileName)
   
-  
   fs.writeFileSync(finalPath, "");
-  // res.redirect("/")
+  res.redirect("/")
 
   // PRINT PATH TO THE CONSOLE 
   console.log(finalPath);
 
-  res.render('create', { finalPath, newFileName })
+  // res.render('create', { finalPath, newFileName })
+})
+
+// router.get('/file/:filename', function (req, res, next) {
+//   console.log(req.params.filename)
+//   // res.send(req.params.filename)
+//   res.render('file', {data: req.params.filename})
+// })
+
+// --------------- READ THE CONTENT OF THE FILE ------
+router.get('/:fileName', function (req, res, next) {
+  let filePath = path.join(__dirname, "..", "public", "uploads", req.params.fileName);
+  let fileContent = fs.readFileSync(filePath, "utf-8")
+
+  console.log(fileContent)
+  res.render('ShowContent', {title: 'FileContent', fileContent, fileName: req.params.fileName})
+})
+
+router.get('/delete/:fileName', function (req, res, next) {
+  let filePath = path.join(__dirname, "..", "public", "uploads", req.params.fileName)
+  // console.log(filePath)  
+  console.log("PARAMS", req.params.fileName)
+  fs.unlinkSync(filePath)
+  // res.send("filePath" + filePath)
+  res.redirect('/')
 })
 
 module.exports = router;
--- 02-Express/readme.md
+++ 02-Express/readme.md
@@ -1,16 +1,17 @@
 # Imp Points
+
 - Port is similar to domain
 - Servers are computer that are 24 hr ON
 
 Different language & their servers:
+
 - Java - tomcat
 - Php - apache
 - Django - khud se banaa hota hai
 - Node - fastest server
 
-
-
 # Installation - Basic Template
+
 npm i --save express
 
 MVC:(Model Views Controller)
@@ -22,13 +23,13 @@ Database Interaction: Managed by Models, typically interfacing with MongoDB.
 ejs: embedded javaScript
 
 FOLDER STRUCTURE/:
+
 - routes
 - views (Contains EJS view templates.)
 - public (Houses controller JavaScript files.)
-    - images
-    - styles
-    - javascripts
-
+  - images
+  - styles
+  - javascripts
 
 # Express Boiler Plate
 
@@ -43,9 +44,10 @@ npm start
 
 <h1><%= title %></h1>
 
-<%- include("header)  %>
+<%- include("header) %>
 
 # -------
+
 for get request - req.query
 for post request - req.body
 
@@ -54,13 +56,20 @@ for post request - req.body
 - Dynamic route ka data show karne ke liye: req params
 
 # ----------
+
 res.send()
 res.json()
 res.render()
 res.redirect()
 
 # How to comment out in ejs file
-    <% /* %> 
+
+    <% /* %>
         <h2>Name: <%= userName %> </h2>
-        <h2>Address: <%= address %> </h2> 
+        <h2>Address: <%= address %> </h2>
     <% */ %>
+
+# Dynamic Route
+<form action="/delete/<%= data %>" method="get">
+
+router.get('/delete/:fileName', function (req, res, next) {
